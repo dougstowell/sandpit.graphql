5ded16918c8355ee326cfd723556c74ada5dd97a
dougstowell <doug.stowell@gmail.com>
2017-09-01 17:56:21 +0100

    Initial DB access.

---
+++ data-access/db.js
@@ -0,0 +1,89 @@
+const Sequelize = require('sequelize');
+const Faker = require('faker');
+const _ = require('lodash');
+
+const Conn = new Sequelize(
+  'blueprint-dev-graphql',
+  'postgres',
+  '', {
+    dialect: 'postgres',
+    host: 'localhost'
+  }
+);
+
+const User = Conn.define('user', {
+  userName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true
+    }
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  },
+});
+
+const Booking = Conn.define('booking', {
+  reason: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+// Relations
+User.hasMany(Booking);
+Booking.belongsTo(User);
+
+Conn.sync({
+  force: true
+}).then(() => {
+  _.times(10, () => {
+    return User.create({
+      userName: Faker.internet.userName(),
+      firstName: Faker.name.firstName(),
+      lastName: Faker.name.lastName(),
+      email: Faker.internet.email(),
+      isAdmin: false
+    }).then(user => {
+      return user.createBooking({
+        reason: `Room booking needed by ${user.firstName}`,
+        description: 'For project stand ups'
+      });
+    });
+  });
+});
+
+function getUsers(filter) {
+  return Conn.models.user.findAll({
+    where: filter
+  });
+}
+
+function getBookings(filter) {
+  return Conn.models.booker.findAll({
+    where: filter
+  });
+}
+
+module.exports = {
+  getUsers: getUsers,
+  getBookings: getBookings
+}
--- index.js
+++ index.js
@@ -1,1 +1,3 @@
+const Db = require('./data-access/db');
+
 console.log('sandpit.graphql ready');
